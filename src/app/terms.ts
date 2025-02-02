7a43b0ba526c7daa9cf7200f3e503f5683cbbb0f
Motidru <motidru@gmail.com>
2025-02-02 11:51:37 +0200

    v2025.02.02

--- src/app/terms.ts
+++ src/app/terms.ts
@@ -1,7 +1,7 @@
 import './common/dateFunc'
 
 export const terms = {
-    appVersion: '2025.01.27',
+    appVersion: '2025.02.02',
     username: "שם",
     signIn: "Sign In",
     confirmPassword: "Confirm Password",
